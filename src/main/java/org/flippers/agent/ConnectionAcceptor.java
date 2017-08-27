9fb0373a07a894910f58bd22b9c82c4f221778fe
Priyank Gupta <priyaaank@gmail.com>
2017-08-27 19:30:03 +0530

    Adding maven to manage dependencies

--- src/main/java/org/flippers/agent/ConnectionAcceptor.java
+++ src/main/java/org/flippers/agent/ConnectionAcceptor.java
@@ -1,11 +1,15 @@
 package org.flippers.agent;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ConnectionAcceptor {
 
     private static ConnectionAcceptor singleInstance;
+    private final Integer port;
 
     private ConnectionAcceptor() {
-
+        this.port = 8752;
     }
 
     public static ConnectionAcceptor instance() {
@@ -15,6 +19,13 @@ public class ConnectionAcceptor {
         return singleInstance;
     }
 
-    
+    public void beginAccepting() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(this.port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
